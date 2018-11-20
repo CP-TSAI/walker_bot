80e42badbd5ac82dd4916193c1d46e8ce4e43c26
Chin-Po <aupt23@gmail.com>
2018-11-20 14:58:29 -0500

    the LICENSE and Doxygen comment is added to the cpp-file

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,26 @@
+/********************************************************************
+ *  MIT License
+ *  Copyright <2018> <Chin-Po Tsai>
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ *  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ *  of the Software.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ *  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *  IN THE SOFTWARE.
+ ********************************************************************/
+
+
+/** @file main.cpp
+ *  @brief publisher publishes velocity commands according to the subscribed sensor information
+ *  @copyright MIT License
+ *  @author Chin-Po Tsai
+ */
+
 #include <sensor_msgs/LaserScan.h>
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
@@ -5,22 +28,25 @@
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "walker");
-
-  walker simpleWalker;
   ros::NodeHandle n;
 
+  /// the object for walker class
+  walker simpleWalker;
+  
+  /// the subscriber for laser
   ros::Subscriber sensor = n.subscribe<sensor_msgs::
           LaserScan>("/scan", 50, &walker::sensorCallback, &simpleWalker);
 
+  /// the publisher for velocity
   ros::Publisher velocity = n.advertise<geometry_msgs::Twist>
                              ("/mobile_base/commands/velocity", 1000);
 
   ros::Rate loop_rate(10);
 
   while (ros::ok()) {
+    
+    /// the velocity command for turtlebot
     geometry_msgs::Twist msg;
-
-    // Initiaizing msg with 0
     msg.linear.x = 0.0;
     msg.linear.y = 0.0;
     msg.linear.z = 0.0;
@@ -28,18 +54,18 @@ int main(int argc, char **argv) {
     msg.angular.y = 0.0;
     msg.angular.z = 0.0;
 
+
+    /// the velocity decision process according to the laser information
     if (simpleWalker.isClose() == true) {
-      // rotate until obstacle out of the range
       msg.angular.z = 1;
     } else {
-      // move forward
       msg.linear.x = 0.3;
     }
 
+    /// publish the velocity
     velocity.publish(msg);
 
     ros::spinOnce();
-
     loop_rate.sleep();
   }
   return 0;
--- src/walker.cpp
+++ src/walker.cpp
@@ -1,14 +1,47 @@
+/********************************************************************
+ *  MIT License
+ *  Copyright <2018> <Chin-Po Tsai>
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ *  documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ *  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *  The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ *  of the Software.
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ *  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *  IN THE SOFTWARE.
+ ********************************************************************/
+
+
+/** @file walker.cpp
+ *  @brief the implementation of the walker algorithm
+ *  @copyright MIT License
+ *  @author Chin-Po Tsai
+ */
+
 #include <stdlib.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include "walker.hpp"
 
+
+/// the constructor of the class
 walker::walker() {
   isObstacle = false;
 }
 
+
+/// the destructor of the class
 walker::~walker() {}
 
+
+/**
+ * @brief A callback function for laser data
+ * @param msg
+ * @return void
+ */
 void walker::sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
     for (auto i = 0; i < msg->ranges.size(); ++i) {
         if (msg->ranges[i] < 0.6) {
@@ -23,6 +56,12 @@ void walker::sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
     return;
 }
 
+
+/**
+ * @brief returns if an obstacle is ahead
+ * @param void
+ * @return bool
+ */
 bool walker::isClose() {
   return isObstacle;
 }
